beeb274880e81adce78a179c701422cc624c6f78
malantivora04 <146733377+malantivora04@users.noreply.github.com>
2023-12-20 16:02:16 +0530

    Initial commit

---
+++ set.js
@@ -0,0 +1,7 @@
+
+function set(){
+    var n = document.getElementById("t1").value ;
+    var msg = document.getElementById("t2").value ;
+
+    document.getElementById('p1').innerHTML = n+":"+ msg ;
+}
